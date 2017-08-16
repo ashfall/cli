acf77745c6ab64e2fc48e9e98190d67fca5dcf48
Ashwini Oruganti <ashwini.oruganti@gmail.com>
2017-08-16 11:21:24 -0700

    Mass rename Sirupsen to sirupsen

--- cli/command/trust/info.go
+++ cli/command/trust/info.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/docker/cli/cli"
 	"github.com/docker/cli/cli/command"
 	"github.com/docker/cli/cli/command/formatter"
